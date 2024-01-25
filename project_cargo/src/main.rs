2ef5369628d73931f5eb115dac24f4aa3cb40f81
YulianSan <knoob0733@gmail.com>
2024-01-25 17:50:24 -0300

    moving game to other file

--- project_cargo/src/main.rs
+++ project_cargo/src/main.rs
@@ -1,38 +1,5 @@
-use rand::Rng;
-use std::{cmp::Ordering, io};
+mod game;
 
 fn main() {
-    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
-    println!("Guess the number!");
-
-    loop {
-        println!("Please input your guess: ");
-
-        let mut guess: String = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        println!("You guessed: {}", guess.trim());
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-        println!();
-    }
+    game::game()
 }
-
