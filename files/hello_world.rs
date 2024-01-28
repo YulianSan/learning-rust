a3e5455c03c096fe724cc54b893f82a780055ac8
YulianSan <knoob0733@gmail.com>
2024-01-28 17:33:07 -0300

    things

--- files/hello_world.rs
+++ files/hello_world.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 0;
+    x = 2;
+    increment(&mut x.clone());
+    println!("x: {x}");
+}
+
+fn increment(x: &mut i32) {
+    *x += 1;
 }
