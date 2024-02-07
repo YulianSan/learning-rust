a803d43a077de8ede0e5a2dbbe0349cc1671f091
YulianSan <knoob0733@gmail.com>
2024-02-06 21:54:30 -0300

    rewrite game guess number

--- project_cargo/src/game/mod.rs
+++ project_cargo/src/game/mod.rs
@@ -1,6 +1,28 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Self {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+pub fn game2() {
+    let x = Guess::new(100);
+    println!("{}", x.value());
+}
+
 pub fn game() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
