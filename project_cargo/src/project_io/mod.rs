ddeb491a1b005b2742b52b4be8eb43a8950b1e17
YulianSan <knoob0733@gmail.com>
2024-02-18 18:37:13 -0300

    using eprintln! to use stderr

--- project_cargo/src/project_io/mod.rs
+++ project_cargo/src/project_io/mod.rs
@@ -4,12 +4,13 @@ mod lib;
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let config = lib::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = lib::run(&config) {
-        println!("Application error {}", e);
+        // macro to print in stderr
+        eprintln!("Application error {}", e);
         process::exit(1);
     }
 }
