e26fbf77ef87a1c4debb31d8cc3638ce0966eb05
YulianSan <knoob0733@gmail.com>
2024-02-18 00:19:29 -0300

    adding more features in mini project and make tests

--- project_cargo/src/project_io/lib.rs
+++ project_cargo/src/project_io/lib.rs
@@ -21,7 +21,46 @@ impl Config {
 
 pub fn run(config: &Config) -> Result<String, std::io::Error> {
     let content = fs::read_to_string(&config.file_path)?;
-    println!("content: {}", content);
+    let matches_search = search(&config.query, &content);
+    println!("\nMatches:\n{}", matches_search.join("\n"));
 
     Ok(content)
+}
+
+pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut matches_search = vec![];
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            matches_search.push(line);
+        }
+    }
+
+    matches_search
+}
+
+// run only in "cargo test"
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // the \ tells to not put a new line
+    const CONTENTS: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    #[test]
+    fn return_one_line() {
+        let query = "afe";
+
+        assert_eq!(vec!["safe, fast, productive."], search(&query, &CONTENTS));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "pICK";
+
+        assert_eq!(vec!["Pick three."], search(&query, CONTENTS));
+    }
 }
