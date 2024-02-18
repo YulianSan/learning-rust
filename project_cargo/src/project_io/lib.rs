7058bbccf75cb36a0ecac9acd208d0cc49615514
YulianSan <knoob0733@gmail.com>
2024-02-18 18:36:29 -0300

    split search on two functions: search and search_case_insensitive

--- project_cargo/src/project_io/lib.rs
+++ project_cargo/src/project_io/lib.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{env, fs};
 
 #[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,19 +16,36 @@ impl Config {
         Ok(Config {
             query: args[1].clone(),
             file_path: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
         })
     }
 }
 
 pub fn run(config: &Config) -> Result<String, std::io::Error> {
     let content = fs::read_to_string(&config.file_path)?;
-    let matches_search = search(&config.query, &content);
+    let matches_search = match config.ignore_case {
+        true => search_case_insensitive(&config.query, &content),
+        false => search(&config.query, &content),
+    };
+
     println!("\nMatches:\n{}", matches_search.join("\n"));
 
     Ok(content)
 }
 
 pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let mut matches_search = vec![];
+
+    for line in content.lines() {
+        if line.contains(&query) {
+            matches_search.push(line);
+        }
+    }
+
+    matches_search
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut matches_search = vec![];
 
@@ -61,6 +79,9 @@ Pick three.";
     fn case_insensitive() {
         let query = "pICK";
 
-        assert_eq!(vec!["Pick three."], search(&query, CONTENTS));
+        assert_eq!(
+            vec!["Pick three."],
+            search_case_insensitive(&query, CONTENTS)
+        );
     }
 }
