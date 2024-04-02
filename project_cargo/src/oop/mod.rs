1ce02c6e6e2c7e75f8da72d4d612596eded0c6b0
YulianSan <knoob0733@gmail.com>
2024-04-01 21:57:58 -0300

    learn more about pointers and smart_pointers

--- project_cargo/src/oop/mod.rs
+++ project_cargo/src/oop/mod.rs
@@ -1,4 +1,19 @@
+use std::rc::Rc;
+
 pub mod encapsulation;
 pub mod gui;
 pub mod patterns;
 pub mod post;
+
+fn example1() {
+    trait Test1 {
+        fn new() -> Self
+        where
+            Self: Sized;
+    }
+
+    // dyn no have lenght, that's why need a pointer to say the address and lenght
+    let x: Vec<Box<dyn Test1>> = vec![];
+    let x1: Vec<&dyn Test1> = vec![];
+    let x1: Vec<Rc<&dyn Test1>> = vec![];
+}
